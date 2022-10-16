506850335317d741a0989f759465aa40f6548e42
phamlinh291291 <phamlinh291291@gmail.com>
2022-10-17 00:24:31 +0700

    OKE!

---
+++ source/hinhtron.js
@@ -0,0 +1,5 @@
+const PI = 3.14;
+let bankinh = prompt("nhập vào bán kính");
+let chuvi = 2 * bankinh * PI;
+let dientich = bankinh * bankinh * PI;
+document.write("S= " + dientich + " , P= " + chuvi);
